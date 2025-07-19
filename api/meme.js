ecd23267f3f2c0cc791e191a1becee431120e5c1
rithwiksrivastav4 <srivastavrithwik@gmail.com>
2025-07-19 15:58:01 +0530

    added chnages

---
+++ api/meme.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const memes = [
+    "https://i.imgflip.com/1bij.jpg",
+    "https://i.redd.it/3t9a3y19glf51.jpg",
+    "https://i.redd.it/8u6h8tq5ehg51.jpg",
+    "https://i.redd.it/2fn5b9l9q6e51.jpg",
+    "https://i.redd.it/vkndh5gij9h51.jpg"
+  ];
+
+  const randomMeme = memes[Math.floor(Math.random() * memes.length)];
+  res.setHeader("Content-Type", "image/jpeg");
+  res.redirect(randomMeme);
+}
